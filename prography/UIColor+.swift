09b74fad5bf737b5fa6ba283526537c4302fc6b5
창혁 <dpkyle97@gmail.com>
2025-02-17 14:51:02 +0900

    Feat: 기본 설정
    - 스토리보드를 사용하지 않기 때문에 Info.plist에서 삭제할부분을 삭제하고, 나머지 부분을 구성

---
+++ prography/UIColor+.swift
@@ -0,0 +1,30 @@
+//
+//  UIColor+.swift
+//  prography
+//
+//  Created by kangChangHyeok on 17/02/2025.
+//
+
+import UIKit
+
+extension UIColor {
+    /// hexString값을 사용하여 색상을 생성한다. alpha 기본값 = 1.0
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: alpha)
+    }
+}
