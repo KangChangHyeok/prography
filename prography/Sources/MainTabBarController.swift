9b805060492758e1ba923b974ac35dc747f89d1e
창혁 <dpkyle97@gmail.com>
2025-03-28 23:18:29 +0900

    Refactor: MainTabBarController 코드 정리
    - viewDidLoad 메서드 안에 함수 기능별로 분류하여 코드 정리

--- prography/Sources/MainTabBarController.swift
+++ prography/Sources/MainTabBarController.swift
@@ -12,42 +12,15 @@ final class MainTabBarController: UITabBarController {
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
-        let HomeNavController = createNavController(
-            for: HomeViewController(),
-            title: "Home",
-            imageName: "House",
-            selectedImageName: "RedHouse"
-        )
-        
-        let myViewModel = MyViewModel()
-        let MyNavController = createNavController(
-            for: MyViewController(viewModel: myViewModel),
-            title: "My",
-            imageName: "Star",
-            selectedImageName: "RedStar"
-        )
-        viewControllers = [HomeNavController, MyNavController]
-        
-        let tabBarAppearance = UITabBarAppearance()
-        tabBarAppearance.backgroundColor = .init(hex: "f2f2f7")
-        tabBarAppearance.configureWithOpaqueBackground()
-        tabBarAppearance.shadowColor = .clear
-        
-        UITabBar.appearance().tintColor = .main
-        UITabBar.appearance().unselectedItemTintColor = .black
-        UITabBar.appearance().standardAppearance = tabBarAppearance
-        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-        
-        let navigationBarAppearance = UINavigationBarAppearance()
-        navigationBarAppearance.backgroundColor = .white
-        navigationBarAppearance.configureWithOpaqueBackground()
-        navigationBarAppearance.shadowColor = .clear
-        
-        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
-        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        configureViewControllers()
+        configureTabBarAppearance()
+        configureNavigationBarAppearance()
     }
-    
-    // MARK: - Helper
+}
+
+// MARK: - Helper
+
+private extension MainTabBarController {
     
     func createNavController(
         for rootViewController: UIViewController,
@@ -67,5 +40,45 @@ final class MainTabBarController: UITabBarController {
         imageView.frame = .init(origin: .init(x: 0, y: 0), size: .init(width: 144, height: 24))
         navController.navigationBar.topItem?.titleView = imageView
         return navController
+    }
+    
+    func configureViewControllers() {
+        let HomeNavController = createNavController(
+            for: HomeViewController(),
+            title: "Home",
+            imageName: "House",
+            selectedImageName: "RedHouse"
+        )
+        
+        let myViewModel = MyViewModel()
+        let MyNavController = createNavController(
+            for: MyViewController(viewModel: myViewModel),
+            title: "My",
+            imageName: "Star",
+            selectedImageName: "RedStar"
+        )
+        viewControllers = [HomeNavController, MyNavController]
+    }
+    
+    func configureTabBarAppearance() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.backgroundColor = .init(hex: "f2f2f7")
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.shadowColor = .clear
+        
+        UITabBar.appearance().tintColor = .main
+        UITabBar.appearance().unselectedItemTintColor = .black
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
+    
+    func configureNavigationBarAppearance() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .white
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.shadowColor = .clear
+        
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
     }
 }
